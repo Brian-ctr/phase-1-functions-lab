abccad8c8d47404ca056288df3706624cbcb10e1
Brian Cheruiyot Rono <cheruiyotbrian97@gmail.com>
2023-03-20 06:53:57 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    if(location===43){
+    return 1
+}
+    else if(location==50 || location<42)
+    return 8
+}
+function distanceFromHqInFeet(location){
+    if(location===43)
+    return 264
+    if(location===50)
+    return 2112
+    if(location===34)
+    return 2112
+}
+function distanceTravelledInFeet(location){
+if(location===43 || location===48)
+    return 1320
+if(location===50 || location===60)
+    return 2640
+if(location<42)
+    return 1584
+}
+function calculatesFarePrice(start, destination){
+    let distance= distanceTravelledInFeet(start, destination)
+    if(distance===43,44){
+function calculatesFarePrice(beginning, end){
+let distance= distanceTravelledInFeet(beginning, end)
+    if(beginning===43 && end===44){
+        return 0;
+    }
+
+    if(distance===34 && distance===32) {
+    return 2.56
+    if(beginning===34 && end===32) {
+    return (((beginning - end)*264)- 400)*0.02
+
+    }
+    if(distance>=2000 && distance <=2500){
+    if(beginning===50 && end ===58){
+    return 25;
+    }
+    else if(distance>2500) {
+        return "cannot travel that far"
+    else {
+        return "cannot travel that far";
+    }
+
+} 
+
